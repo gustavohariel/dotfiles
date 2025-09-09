7b70e21fc84af4b1c1dd8ec34b7b01626f5b977b
Gustavo Hariel <g_hariel@icloud.com>
2025-09-09 13:05:54 -0300

    chore: update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,110 @@
--- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
--- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+local utils = require("utils")
+
+local colorscheme = "custom"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- validate that lazy is available
-if not pcall(require, "lazy") then
-  -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
+-- vim opts
+require("vimopts")
 
-require "lazy_setup"
-require "polish"
+-- lazy.nvim setup
+require("lazy").setup("plugins", {
+  defaults = {
+    lazy = false,
+  },
+})
+
+vim.filetype.add({ extension = { templ = "templ" } })
+vim.filetype.add({ extension = { purs = "purescript" } })
+vim.filetype.add({
+  extension = { nim = "nim" },
+  filename = { ["Nim"] = "nim" },
+})
+
+-- stop zig qf list from opening >:(
+vim.g.zig_fmt_parse_errors = 0
+
+-- treesitter config
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ignore_install = {},
+  ensure_installed = {
+    "typst",
+    "purescript",
+    "nix",
+    "nim",
+    "vimdoc",
+    "go",
+    "rust",
+    "c",
+    "lua",
+    "python",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "prisma",
+    "haskell",
+    "zig",
+    "gleam",
+    "wgsl",
+    "php",
+    "nim",
+    "sql",
+    "markdown",
+    "latex",
+    "gdscript",
+    "gdshader",
+  },
+  highlight = {
+    enable = true,
+  },
+  indent = { enable = true },
+  modules = {},
+  sync_install = true,
+  auto_install = true,
+})
+
+utils.color_overrides.setup_colorscheme_overrides()
+
+-- theme
+vim.cmd("colorscheme " .. colorscheme)
+
+utils.fix_telescope_parens_win()
+utils.dashboard.setup_dashboard_image_colors()
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 2
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "csv",
+  callback = function()
+    vim.cmd("CsvViewEnable display_mode=border")
+    vim.opt_local.wrap = false
+  end,
+})
--- nvim/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- {
-  --   "rebelot/kanagawa.nvim",
-  --   priority = 1000, -- Load it early to set the colorscheme
-  --   config = function() vim.cmd "colorscheme kanagawa" end,
-  -- },
-  "nyoom-engineering/oxocarbon.nvim",
-}
