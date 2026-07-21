5796c6942a06946ded5044e6e79e46f7895af75d
Gustavo Hariel <g_hariel@icloud.com>
2026-07-21 19:17:03 -0300

    nvim: show hidden/ignored files in snacks picker

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,9 +3,11 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        hidden = true,
-        ignored = true,
         sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
           explorer = {
             hidden = true,
             ignored = true,
